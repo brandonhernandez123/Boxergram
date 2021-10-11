63f949dd9176471e016ae1f77bf7e7373dc6b1cf
Brandon Hernandez <brandoncinthia0621@gmail.com>
2021-10-11 15:43:02 -0600

    completed comments controllers

---
+++ controllers/CommentController.js
@@ -0,0 +1,87 @@
+const { User, Post, Event, Comment } = require('../models')
+
+const GetAllComments = async (req, res) => {
+  try {
+    const events = await Comment.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['first_name', 'last_name', 'profile_picture']
+        },
+        {
+          model: Post
+        },
+        {
+          model: Event
+        }
+      ]
+    })
+    res.send(events)
+  } catch (error) {
+    throw error
+  }
+}
+
+const NewComment = async (req, res) => {
+  try {
+    const newcomment = await Comment.create(req.body)
+    res.send(newcomment)
+  } catch (error) {
+    throw error
+  }
+}
+
+const GetOneComment = async (req, res) => {
+  try {
+    const comment = await Comment.findByPk(req.params.comment_id, {
+      include: [
+        {
+          model: User,
+          attributes: ['first_name', 'last_name', 'profile_picture']
+        },
+        {
+          model: Post
+        },
+        {
+          model: Event
+        }
+      ]
+    })
+    res.send(comment)
+  } catch (error) {
+    throw error
+  }
+}
+
+const DeleteComment = async (req, res) => {
+  try {
+    await Comment.destroy({ where: { id: req.params.comment_id } })
+    res.send({
+      msg: 'Comment Deleted',
+      payload: req.params.comment_id,
+      status: 'Ok'
+    })
+  } catch (error) {
+    throw error
+  }
+}
+
+const UpdateComment = async (req, res) => {
+  try {
+    const comment = await Comment.update(
+      { ...req.body },
+      { where: { id: req.params.comment_id }, returning: true }
+    )
+    res.send(comment)
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  UpdateComment,
+  GetAllComments,
+  GetOneComment,
+  DeleteComment,
+  NewComment
+}
