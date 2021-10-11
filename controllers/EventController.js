4d73f04ec00beb508d7507b5c1c55cde12ee08cf
Brandon Hernandez <brandoncinthia0621@gmail.com>
2021-10-11 15:01:52 -0600

    completed controller for events

---
+++ controllers/EventController.js
@@ -0,0 +1,75 @@
+const { User, Post, Event, Comment } = require('../models')
+
+const GetAllEvents = async (req, res) => {
+  try {
+    const events = await Event.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['first_name', 'last_name', 'profile_picture']
+        }
+      ]
+    })
+    res.send(events)
+  } catch (error) {
+    throw error
+  }
+}
+
+const NewEvent = async (req, res) => {
+  try {
+    const newevent = await Event.create(req.body)
+    res.send(newevent)
+  } catch (error) {
+    throw error
+  }
+}
+
+const GetOneEvent = async (req, res) => {
+  try {
+    const EventandUser = await Event.findByPk(req.params.event_id, {
+      include: [
+        {
+          model: User,
+          attributes: ['first_name', 'last_name', 'profile_picture']
+        }
+      ]
+    })
+    res.send(EventandUser)
+  } catch (error) {
+    throw error
+  }
+}
+
+const DeleteEvent = async (req, res) => {
+  try {
+    await Event.destroy({ where: { id: req.params.event_id } })
+    res.send({
+      msg: 'Post Deleted',
+      payload: req.params.event_id,
+      status: 'Ok'
+    })
+  } catch (error) {
+    throw error
+  }
+}
+
+const UpdateEvent = async (req, res) => {
+  try {
+    const event = await Event.update(
+      { ...req.body },
+      { where: { id: req.params.event_id }, returning: true }
+    )
+    res.send(event)
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  GetAllEvents,
+  NewEvent,
+  GetOneEvent,
+  DeleteEvent,
+  UpdateEvent
+}
