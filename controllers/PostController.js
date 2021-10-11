0425133893363e31733753aa806b2a86700b0c46
Brandon Hernandez <brandoncinthia0621@gmail.com>
2021-10-11 14:12:02 -0600

    completed posts controller

---
+++ controllers/PostController.js
@@ -0,0 +1,68 @@
+const { User, Post, Event } = require('../models')
+
+const GetAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.findAll()
+    res.send(posts)
+  } catch (error) {
+    throw error
+  }
+}
+
+const NewPost = async (req, res) => {
+  try {
+    const newpost = await Post.create(req.body)
+    res.send(newpost)
+  } catch (error) {
+    throw error
+  }
+}
+
+const GetOnePost = async (req, res) => {
+  try {
+    const PostsandUser = await Post.findByPk(req.params.user_id, {
+      include: [
+        {
+          model: User,
+          attributes: ['first_name', 'last_name', 'profile_picture']
+        }
+      ]
+    })
+    res.send(PostsandUser)
+  } catch (error) {
+    throw error
+  }
+}
+
+const DeletePost = async (req, res) => {
+  try {
+    await Post.destroy({ where: { id: req.params.post_id } })
+    res.send({
+      msg: 'Post Deleted',
+      payload: req.params.post_id,
+      status: 'Ok'
+    })
+  } catch (error) {
+    throw error
+  }
+}
+
+const UpdatePost = async (req, res) => {
+  try {
+    const post = await Post.update(
+      { ...req.body },
+      { where: { id: req.params.post_id }, returning: true }
+    )
+    res.send(post)
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  GetAllPosts,
+  NewPost,
+  GetOnePost,
+  DeletePost,
+  UpdatePost
+}
