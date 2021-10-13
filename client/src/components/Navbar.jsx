1df38958a7baae97570c2a0f76305897688b9730
Brandon Hernandez <brandoncinthia0621@gmail.com>
2021-10-12 21:08:44 -0600

    mobile friendly navbar created

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {Nav, Navbar, Container} from 'react-bootstrap'
+
+
+
+const Navigation = () => {
+    return(
+        <footer>
+        <Navbar fixed='bottom' bg="light" variant="light" className='navbar' >
+        <Container>
+        <Navbar.Brand href="#home"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
+  <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"/>
+</svg></Navbar.Brand>
+        <Nav className="justify-content-center" id="newpost">
+        <Nav.Link  href="#features"><svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+</svg></Nav.Link>
+        </Nav> 
+        <Nav className="justify-content-end">
+        <Nav.Link><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+</svg></Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      </footer>
+    )
+}
+
+
+export default Navigation
