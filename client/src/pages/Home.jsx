f8b425efe3863d9ea4e1cf3eab096dc235bce2de
Brandon Hernandez <brandoncinthia0621@gmail.com>
2021-10-16 23:50:14 -0600

    styling home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,16 +4,16 @@ import React from 'react'
 
 const Home = () => {
     return(
-        <div className='home'>
+        <div className='newevent'>
            <h1>This is BoxerGram</h1>
            <p>BoxerGram is a platform for Boxers, Coaches and fanatics 
                to share their favorite pictures related to boxing</p>
 
-            <section className='home'>
+           
                 <h2>This App was made by a boxing fan for boxing fans</h2>
                 <p> This app was created by me (Brandon Hernandez)</p>
                 
-            </section>
+           
         </div>
     )
 }
