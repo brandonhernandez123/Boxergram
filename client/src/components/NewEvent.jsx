605567dd82039cc3c588a0983067d25ab0aa407b
Brandon Hernandez <brandoncinthia0621@gmail.com>
2021-10-16 22:21:34 -0600

    refactoring db

--- client/src/components/NewEvent.jsx
+++ client/src/components/NewEvent.jsx
@@ -50,25 +50,25 @@ return(
       <Form onSubmit={OnSubmit}>
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Event Title</Form.Label>
-    <Form.Control type="input" required='true' value={NewEvent.title} handleChange={handleChangeTitle} name='title' placeholder="What is the name of your event" />
+    <Form.Control type="input" required='true'  onChange={handleChangeTitle} name='title' placeholder="What is the name of your event" />
     <Form.Text className="text-muted">
 Give your event an engaging name    </Form.Text>
   </Form.Group>
 
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Description</Form.Label>
-    <Form.Control type="input" required='true' value={NewEvent.description} handleChange={handleChangeDescription} name='description' placeholder="Give a brief description of your event and when it takes place" />
+    <Form.Control type="input" required='true'  onChange={handleChangeDescription} name='description' placeholder="Give a brief description of your event and when it takes place" />
   </Form.Group>
 
 
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Image</Form.Label>
-    <Form.Control type="input" required='true' value={NewEvent.image} handleChange={handleChangeImage} name='Image' placeholder="Include an image url for you event" />
+    <Form.Control type="input" required='true'  onChange={handleChangeImage} name='Image' placeholder="Include an image url for you event" />
   </Form.Group>
 
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Location</Form.Label>
-    <Form.Control type="input" required='true' value={NewEvent.location} handleChange={handleChangeLocation} name='location' placeholder="Where is your Event located at include the entire address" />
+    <Form.Control type="input" required='true'  onChange={handleChangeLocation} name='location' placeholder="Where is your Event located at include the entire address" />
   </Form.Group>
 
   <Button disabled={!NewEvent.title || !NewEvent.image || !NewEvent.description || !NewEvent.location } variant="primary" type="submit">
