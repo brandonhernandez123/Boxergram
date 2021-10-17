b13adf0c3743322f0341b90db32b7c446f69c961
Brandon Hernandez <brandoncinthia0621@gmail.com>
2021-10-16 19:54:47 -0600

    set up useState for modal event

--- client/src/components/NewEvent.jsx
+++ client/src/components/NewEvent.jsx
@@ -1,1 +1,21 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Client from '../services'
+
+
+
+const NewEvent = (post) => {
+    const [NewEvent, SetNewEvent] = useState({
+        title: '',
+        description: '',
+        image: '',
+        location: ''
+    })
+}
+
+
+
+
+
+
+
+export default NewEvent
