fb0a2624bec59eca9806b4f7825aa7ee8a056b46
Brandon Hernandez <brandoncinthia0621@gmail.com>
2021-10-17 21:37:01 -0600

    added basic styling to myprofile page

--- client/src/pages/MyProfile.jsx
+++ client/src/pages/MyProfile.jsx
@@ -1,6 +1,6 @@
 import React,{useState, useEffect} from 'react'
 import Client from '../services'
-import {Row,Col,Container,Image,Card,Button} from 'react-bootstrap'
+import {Row,Col,Container,Image,Card,Button, Alert} from 'react-bootstrap'
 
 
 
@@ -37,6 +37,8 @@ const MyProfile = (props) => {
     const DeletePost = async (index) => {
         const id = `${userPosts[index].id}`
           const res = Client.delete(`/deletepost/${id}`)
+          alert('Post Deleted')
+          props.history.push('/feed')
         }
     
     console.log(userPosts.title)
@@ -48,27 +50,37 @@ const MyProfile = (props) => {
       <Image  className='profilepic'  src={`${getProfile.profile_picture}`} roundedCircle />
       <h2>{getProfile.first_name} {getProfile.last_name}</h2>
       <h4>{getProfile.email}</h4>
-<br/>
-<br/>
-<h2>My Posts</h2>
-{userPosts.map((post,index) => (
-    <div className='userposts'>
 
-      <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={`${post.image}`} />
-  <Card.Body className='newevent'>
-    <Card.Title >{post.title}</Card.Title>
-    <Card.Text>
-     {post.caption}
-    </Card.Text>
-    <Button variant="danger" onClick={()=>DeletePost(index)}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-</svg></Button>
-    <Button variant="info" >Update Post</Button>
-  </Card.Body>
-</Card>
-</div>
-))}
+<br/>
+{userPosts.map((post,index) => (
+    <div className="postcard">
+        <Row xs={1} md={1} className="g-4">
+  {Array.from({ length: 1 }).map((_, idx) => (
+    <Col>
+      <Card>
+          <Button onClick={()=>DeletePost(index)} variant='danger'>Delete</Button>
+        <Card.Img variant="top" src={`${post.image}`} />
+        <Card.Body className='newevent'>
+          <Card.Title> {post.title}</Card.Title>
+          <Card.Text>
+            {post.caption}
+          </Card.Text>  
+         
+        
+        </Card.Body>
+      
+      </Card>
+      
+    </Col>
+    
+  ))}
+</Row>
+    </div>
+         
+        ))}
+   
+
+
 
     </Col>
    
